8f74e7183d3c9cfcee8b53e229095b0689568dd3
Erdem Uslu <usluerdem@yandex.com>
2019-05-16 16:09:30 +0300

    redesign User schema

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Koa = require('koa')
 const BodyParser = require('koa-bodyparser')
 const limit = require('koa-limit')
+const cors = require('koa-cors')
 
 // load db
 const db = require('./helper/db')
@@ -16,6 +17,9 @@ app.use(limit({
   limit: 1000,
   interval: 1000 * 60 * 60
 }))
+
+// cors
+app.use(cors())
 
 // Use the bodyparser middlware
 app.use(BodyParser())
